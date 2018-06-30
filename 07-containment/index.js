cea6a1d68bf5c8357d91fba6a5cf6dc1773f4a94
twknab <20636750+twknab@users.noreply.github.com>
2018-06-30 11:38:15 -0700

    added lsson on containment

--- 07-containment/index.js
+++ 07-containment/index.js
@@ -23,3 +23,32 @@ function WelcomeDialog() {
   );
 }
 
+
+
+// While this is less common, sometimes you might need multiple “holes” in a component. In such cases you may come up with your own convention instead of using children:
+
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
+      </div>
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <SplitPane
+      left={
+        <Contacts />
+      }
+      right={
+        <Chat />
+      } />
+  );
+}
+
