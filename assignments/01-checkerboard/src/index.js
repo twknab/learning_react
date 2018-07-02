6aa8161d05301e6bfb1b75b479f3865e956f3546
twknab <20636750+twknab@users.noreply.github.com>
2018-07-01 21:33:01 -0700

    added stylings to react project

--- assignments/01-checkerboard/src/index.js
+++ assignments/01-checkerboard/src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import s from './css/styles.css';
 
 class CheckerBoard extends React.Component {
  
   constructor(props) {
     super(props);
-    this.state ={matrixSize: prompt("Checkboard size?")};
+    this.state = {matrixSize: prompt("Checkboard size?")};
+
+    this.state.numbers = [];
+    for (let i = 0; i < this.state.matrixSize; i++) {
+      this.state.numbers.push(i);
+    }
+
+    console.log(this.state.numbers)
+
   }
 
   render(){
     return (
       <div>
         <div id="wrapper">
-          {/* Do Stuff} */}
+          {
+            this.state.numbers.map((number) => <Row value={number} />)
+          }
         </div>
       </div>
     )
@@ -27,7 +38,9 @@ class Row extends React.Component {
   }
 
   render() {
-    return true;
+    return (
+      <div></div>
+    )
   }
 
 }
